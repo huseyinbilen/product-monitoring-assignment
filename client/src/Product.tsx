8e1e72710c745a69b93aece1d0ebc2d499c5fd1c
Huseyin Bilen <huseyinxbilen@gmail.com>
2023-07-09 19:48:44 +0300

    Product API Added

--- client/src/Product.tsx
+++ client/src/Product.tsx
@@ -1,23 +1,37 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Table, Button, Modal, Form, Input, Select } from "antd";
 import Navbar from "./Navbar";
 
 const { Option } = Select;
 
 interface Product {
-  id: string;
-  name: string;
-  category: string;
-  amount: number;
-  currency: string;
-  company: string;
+  _id: string;
+  productName: string;
+  productCategory: string;
+  productAmount: number;
+  amountUnit: string;
+  company: Company;
+}
+
+interface Company {
+  _id: string;
+  companyName: string;
+  companyLegalNumber: string;
+  incorporationCountry: string;
+  website: string;
 }
 
 function ProductTable(): JSX.Element {
+  const [companies, setCompanies] = useState<Company[]>([]);
   const [products, setProducts] = useState<Product[]>([]);
   const [visible, setVisible] = useState(false);
   const [form] = Form.useForm();
   const [editingProduct, setEditingProduct] = useState<Product | null>(null);
+
+  useEffect(() => {
+    fetchCompanies();
+    fetchProducts();
+  }, []);
 
   const showModal = (): void => {
     setVisible(true);
@@ -28,25 +42,129 @@ function ProductTable(): JSX.Element {
     setEditingProduct(null);
   };
 
+  const fetchCompanies = async () => {
+    try {
+      const response = await fetch("http://localhost:3001/company/get-companies", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + document.cookie.split("=")[1],
+        }
+      });
+      const data = await response.json();
+      setCompanies(data.companies);
+      console.log(data)
+    } catch (error) {
+      console.error("Failed to fetch companies:", error);
+    }
+  };
+
+  const fetchProducts = async () => {
+    try {
+      const response = await fetch("http://localhost:3001/product/get-products", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + document.cookie.split("=")[1],
+        }
+      });
+      const data = await response.json();
+      setProducts(data.products);
+      console.log(data)
+    } catch (error) {
+      console.error("Failed to fetch products:", error);
+    }
+  };
+
+  const addProduct = async (product: Product) => {
+    try {
+      const response = await fetch("http://localhost:3001/product/add", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + document.cookie.split("=")[1],
+        },
+        body: JSON.stringify({
+          productName: product.productName,
+          productCategory: product.productCategory,
+          productAmount: product.productAmount,
+          amountUnit: product.amountUnit,
+          company: product.company
+        }),
+      });
+      const data = await response.json();
+      console.log(data);
+      setProducts([...products, data.product]);
+      // fetchCompanies();
+    } catch (error) {
+      console.error("Failed to add product:", error);
+    }
+  };
+
+  const deleteProduct = async (id: string) => {
+    try {
+      console.log(id);
+      await fetch(`http://localhost:3001/product/delete`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + document.cookie.split("=")[1],
+        },
+        body: JSON.stringify({
+          _id: id
+        }),
+      });
+      const updatedProducts = products.filter((product) => product._id !== id);
+      setProducts(updatedProducts);
+    } catch (error) {
+      console.error("Failed to delete product:", error);
+    }
+  }
+
+  const updateProduct = async (product: Product) => {
+    try {
+      const response = await fetch(`http://localhost:3001/product/update`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + document.cookie.split("=")[1],
+        },
+        body: JSON.stringify({
+          _id: product._id,
+          productName: product.productName,
+          productCategory: product.productCategory,
+          productAmount: product.productAmount,
+          amountUnit: product.amountUnit,
+          company: product.company
+
+        }),
+      });
+      const data = await response.json();
+      const updatedProducts = products.map((c) => (c._id === data.id ? data : c));
+      setProducts(updatedProducts);
+      fetchProducts();
+    } catch (error) {
+      console.error("Failed to update products:", error);
+    }
+  }
+
   const handleSubmit = (): void => {
     form.validateFields().then((values) => {
       if (editingProduct) {
-        const updatedProducts = products.map((product) =>
-          product.id === editingProduct.id ? { ...product, ...values } : product
-        );
-        setProducts(updatedProducts);
+        const updatedProduct: Product = { ...editingProduct, ...values };
+        updateProduct(updatedProduct);
       } else {
         const newProduct: Product = {
           id: String(products.length + 1),
           ...values,
         };
-        setProducts([...products, newProduct]);
+        addProduct(newProduct);
       }
       form.resetFields();
       setVisible(false);
       setEditingProduct(null);
     });
-  };
+  };  
 
   const handleEdit = (record: Product): void => {
     form.setFieldsValue(record);
@@ -55,27 +173,28 @@ function ProductTable(): JSX.Element {
   };
 
   const handleDelete = (id: string): void => {
-    const updatedProducts = products.filter((product) => product.id !== id);
-    setProducts(updatedProducts);
+    deleteProduct(id);
   };
 
   const columns = [
-    { title: "Ürün Adı", dataIndex: "name", key: "name" },
-    { title: "Ürün Kategorisi", dataIndex: "category", key: "category" },
-    { title: "Ürün Tutarı", dataIndex: "amount", key: "amount" },
-    { title: "Tutar Birimi", dataIndex: "currency", key: "currency" },
-    { title: "Şirket", dataIndex: "company", key: "company" },
+    { title: "Product Name", dataIndex: "productName", key: "productName" },
+    { title: "Product Category", dataIndex: "productCategory", key: "productCategory" },
+    { title: "Product Amount", dataIndex: "productAmount", key: "productAmount" },
+    { title: "Amount Unit", dataIndex: "amountUnit", key: "amountUnit" },
+    { title: "Company", dataIndex: "company", key: "company",
+    render: (text: string, record: Product) => record.company?.companyName,
+    },
     {
-      title: "İşlemler",
+      title: "Actions",
       dataIndex: "actions",
       key: "actions",
       render: (_: any, record: Product) => (
         <>
           <Button type="link" onClick={() => handleEdit(record)}>
-            Düzenle
+            Edit
           </Button>
-          <Button type="link" danger onClick={() => handleDelete(record.id)}>
-            Sil
+          <Button type="link" danger onClick={() => handleDelete(record._id)}>
+            Delete
           </Button>
         </>
       ),
@@ -87,12 +206,12 @@ function ProductTable(): JSX.Element {
       {/* <Navbar></Navbar> */}
       <div style={{ padding: "20px" }}>
         <Button type="primary" onClick={showModal}>
-          Yeni Ürün Ekle
+          Add New Product
         </Button>
         <Table dataSource={products} columns={columns} rowKey="id" />
 
         <Modal
-          title={editingProduct ? "Ürün Düzenle" : "Yeni Ürün Ekle"}
+          title={editingProduct ? "Edit Product" : "Add New Product"}
           visible={visible}
           onCancel={handleCancel}
           onOk={handleSubmit}
@@ -100,30 +219,30 @@ function ProductTable(): JSX.Element {
         >
           <Form form={form} layout="vertical">
             <Form.Item
-              label="Ürün Adı"
-              name="name"
-              rules={[{ required: true, message: "Ürün adını girin" }]}
+              label="Product Name"
+              name="productName"
+              rules={[{ required: true, message: "Please enter the product name" }]}
             >
               <Input />
             </Form.Item>
             <Form.Item
-              label="Ürün Kategorisi"
-              name="category"
-              rules={[{ required: true, message: "Ürün kategorisini seçin" }]}
+              label="Product Category"
+              name="productCategory"
+              rules={[{ required: true, message: "Please select the product category" }]}
             >
               <Input />
             </Form.Item>
             <Form.Item
-              label="Ürün Tutarı"
-              name="amount"
-              rules={[{ required: true, message: "Ürün tutarını girin" }]}
+              label="Product Amount"
+              name="productAmount"
+              rules={[{ required: true, message: "Please enter the product amount" }]}
             >
               <Input type="number" />
             </Form.Item>
             <Form.Item
-              label="Tutar Birimi"
-              name="currency"
-              rules={[{ required: true, message: "Tutar birimini seçin" }]}
+              label="Amount Unit"
+              name="amountUnit"
+              rules={[{ required: true, message: "Please select the amount unit" }]}
             >
               <Select>
                 <Option value="TRY">TRY</Option>
@@ -132,11 +251,17 @@ function ProductTable(): JSX.Element {
               </Select>
             </Form.Item>
             <Form.Item
-              label="Şirket"
+              label="Company"
               name="company"
-              rules={[{ required: true, message: "Şirketi girin" }]}
+              rules={[{ required: true, message: "Please enter the company" }]}
             >
-              <Input />
+              <Select>
+                {companies.map((company) => (
+                  <Option key={company._id} value={company._id.toString()}>
+                    {company.companyName}
+                  </Option>
+                ))}
+              </Select>
             </Form.Item>
           </Form>
         </Modal>
